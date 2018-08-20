50ecc2b2718f0a2355d56eb024df02423114182e
David Thornton <39091486+dathornton717@users.noreply.github.com>
2018-08-19 20:16:28 -0700

    embed database (#7)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 import java.io.File;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.Optional;
+
+import constants.DatabaseConstants;
+import factory.DataSource;
 import org.apache.catalina.startup.Tomcat;
 
 public class Main {
@@ -7,8 +12,9 @@ public class Main {
     public static final Optional<String> port = Optional.ofNullable(System.getenv("PORT"));
 
     public static void main(String[] args) throws Exception {
-        String docBase = "src/main/webapp/";
+        setUpDatabase();
 
+        String docBase = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
         String webPort = System.getenv("PORT");
         if(webPort == null || webPort.isEmpty()) {
@@ -21,5 +27,13 @@ public class Main {
 
         tomcat.start();
         tomcat.getServer().await();
+    }
+
+    private static void setUpDatabase() throws Exception {
+        Connection connection = DataSource.createDatabaseConnection();
+        for (String command : DatabaseConstants.CREATE_TABLES) {
+            Statement statement = connection.createStatement();
+            statement.execute(command);
+        }
     }
 }
